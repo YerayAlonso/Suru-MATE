9ffd4562867b3a486dc02d7c1fbef567df1485a5
Will Elliott <willscreel@gmail.com>
2018-02-14 08:45:55 -0700

    removed -tint from -brightness-contrast command

--- install.sh
+++ install.sh
@@ -9,21 +9,21 @@ patch -p0 < index.theme.patch
 for file in suru-icon-theme/Suru/*x*/places/folder*
 do
   mogrify -colorspace gray "$file"
-  mogrify -brightness-contrast 7x0 -tint 100 "$file"
+  mogrify -brightness-contrast 7x0 "$file"
   mogrify -fill "#87A752" -tint 100 "$file"
 done
 
 for file in suru-icon-theme/Suru/*x*/places/user*
 do
   mogrify -colorspace gray "$file"
-  mogrify -brightness-contrast 7x0 -tint 100 "$file"
+  mogrify -brightness-contrast 7x0 "$file"
   mogrify -fill "#87A752" -tint 100 "$file"
 done
 
 for file in suru-icon-theme/Suru/*x*/status/folder*
 do
   mogrify -colorspace gray "$file"
-  mogrify -brightness-contrast 7x0 -tint 100 "$file"
+  mogrify -brightness-contrast 7x0 "$file"
   mogrify -fill "#87A752" -tint 100 "$file"
 done
 
