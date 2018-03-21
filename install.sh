afca986018450a5c8eb34851e6942d9722f67d63
Yeray <norike@gmail.com>
2018-03-21 11:09:00 +0100

    Installing SassC and LibSass

--- install.sh
+++ install.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+CURRENTFOLDER=${PWD}
+
+printf '\nInstalling Prerequisites:'
+apt install git build-essential
+
+printf '\nDownloading and installing SassC and LibSass:'
+cd /usr/local/lib/
+git clone https://github.com/sass/sassc.git --branch 3.2.1 --depth 1
+git clone https://github.com/sass/libsass.git --branch 3.2.1 --depth 1
+
+cd /usr/local/lib/sassc/
+export SASS_LIBSASS_PATH="/usr/local/lib/libsass"
+make
+
+cd /usr/local/bin/
+ln -s ../lib/sassc/bin/sassc sassc
+
+cd ${CURRENTFOLDER}
+
+printf '\nDownloading and installing Suru icons:'
 mkdir $HOME/.local/share/icons
 
 git clone https://github.com/snwh/suru-icon-theme.git
